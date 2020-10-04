c5211d2a394f0ab4d021ec94533a864d6dac5908
ccgauche <gaucheron.laurent@gmail.com>
2020-10-04 15:28:55 +0200

    Updated to match Enter key

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,10 @@ window.addEventListener("keyup", (e) => {
         check();
     }
 })
+for (const i of document.getElementsByTagName("input")) {
+    i.addEventListener("keyup", (e) => {
+        if (e.key == "Enter" && document.getElementsByClassName("swal2-shown").length === 0) {
+            check();
+        }
+    })
+}
